ee64a7d3739f8d15d3a0ff0b6af56202dbf658d1
aarturio <106455653+aarturio@users.noreply.github.com>
2023-02-21 19:35:29 -0500

    improved code structure

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
-// Define fetch
-class EasyHTTP {
-  // GET request
-  async get(url) {
+// Init local fetch
+async function fetchData(url) {
+  try {
     const response = await fetch(url);
     const resData = await response.json();
     return resData;
+  } catch (error) {
+    console.log(error);
   }
-}
-
-// Init local fetch
-const http = new EasyHTTP();
-
-// Define UI variables
-const btn1 = document.querySelector("#vbtn-radio1");
-
-// Load event listener
-btn1.addEventListener("click", buildMonthlyFilteredChart);
-
-// Build monthly filtered chart
-function buildMonthlyFilteredChart() {
-  http
-    .get("./office-data.json")
-    .then((data) => {
-      monthlyFilteredChart(JSON.parse(data));
-    })
-    .catch((err) => console.log(err));
-}
-
-// Update monthly filtered chart
-function changeMonthFilteredChart(e) {
-  // Get month value
-  const newMonth = document.querySelector("#month-group").value;
-
-  // Update chart
-  http
-    .get("./office-data.json")
-    .then((data) => {
-      monthlyFilteredChart(JSON.parse(data), newMonth);
-    })
-    .catch((err) => console.log(err));
 }
 
 // Unique function
@@ -46,105 +14,151 @@ function uniqueValues(value, index, self) {
   return self.indexOf(value) === index;
 }
 
-// Monthly filtered data processor
-function monthData(data, selectMonth) {
-  const targetDate = new Date(selectMonth).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-  });
+// Define UI variables
+const btn1 = document.querySelector("#vbtn-radio1");
+const btn2 = document.querySelector("#vbtn-radio2");
+const btn3 = document.querySelector("#vbtn-radio3");
 
-  const filteredData = data.filter((row) => {
-    const tmpDate = new Date(row.date).toLocaleDateString("en-US", {
+// Load event listener
+btn1.addEventListener("click", buildMonthlyFilteredChart);
+btn2.addEventListener("click", buildmonthlyHistoricalChart);
+btn3.addEventListener("click", buildMonthlyFilteredAverageChart);
+
+// Build monthly filtered chart
+buildMonthlyFilteredChart();
+
+function buildMonthlyFilteredChart() {
+  fetchData("./office-data.json")
+    .then((data) => {
+      monthlyFilteredChart(JSON.parse(data));
+    })
+    .catch((err) => console.log(err));
+}
+
+// Update monthly filtered chart
+function changeMonthFilteredChart() {
+  // Get month value
+  const newMonth = document.querySelector("#month-group").value;
+
+  // Update chart
+  fetchData("./office-data.json")
+    .then((data) => {
+      monthlyFilteredChart(JSON.parse(data), newMonth);
+    })
+    .catch((err) => console.log(err));
+}
+
+// Build monthly historical chart
+function buildmonthlyHistoricalChart() {
+  fetchData("./office-data.json")
+    .then((data) => {
+      monthlyHistoricalChart(JSON.parse(data));
+    })
+    .catch((err) => console.log(err));
+}
+
+function buildMonthlyFilteredAverageChart() {
+  fetchData("./office-data.json")
+    .then((data) => {
+      monthlyFilteredAverageChart(JSON.parse(data));
+    })
+    .catch((err) => console.log(err));
+}
+
+function changeMonthFilteredAveragehart() {
+  // Get month value
+  const newMonth = document.querySelector("#month-group").value;
+
+  // Update chart
+  fetchData("./office-data.json")
+    .then((data) => {
+      monthlyFilteredAverageChart(JSON.parse(data), newMonth);
+    })
+    .catch((err) => console.log(err));
+}
+
+// Create a date object from string
+function formatDate(date, type) {
+  if (type === "month")
+    return new Date(date).toLocaleDateString("en-US", {
       year: "numeric",
       month: "long",
     });
-
-    if (tmpDate === targetDate) {
-      return row;
-    }
-  });
-
-  // Dataset class
-  class cityData {
-    constructor(label, borderColor, backgroundColor, data) {
-      this.label = label;
-      this.borderColor = borderColor;
-      this.backgroundColor = backgroundColor;
-      this.data = data;
-      this.tension = 0.1;
-      this.pointRadius = 0;
-      this.borderWidth = 2;
-    }
+  else if (type === "day") {
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    });
+  } else if (type === "weekday") {
+    return new Date(date).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
   }
+}
 
-  const dataATL = new cityData(
-    (label = "Atlanta"),
-    (borderColor = "#4dc9f6"),
-    (backgroundColor = "#4dc9f6"),
-    (data = [])
-  );
+// Monthly filtered data processor
+function getMonthData(monthData, selectMonth) {
+  const targetDate = formatDate(selectMonth, "month");
 
-  const dataBOS = new cityData(
-    (label = "Boston"),
-    (borderColor = "#f67019"),
-    (backgroundColor = "#f67019"),
-    (data = [])
-  );
-
-  const dataPOR = new cityData(
-    (label = "Portand"),
-    (borderColor = "#f53794"),
-    (backgroundColor = "#f53794"),
-    (data = [])
-  );
-
-  const dataSEA = new cityData(
-    (label = "Seattle"),
-    (borderColor = "#537bc4"),
-    (backgroundColor = "#537bc4"),
-    (data = [])
-  );
-
-  filteredData.forEach((row) => {
-    dataATL.data.push({
-      x: new Date(row.date).toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-      }),
-      y: row.Atlanta,
-    });
-    dataBOS.data.push({
-      x: new Date(row.date).toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-      }),
-      y: row.Boston,
-    });
-    dataPOR.data.push({
-      x: new Date(row.date).toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-      }),
-      y: row.Portland,
-    });
-    dataSEA.data.push({
-      x: new Date(row.date).toLocaleDateString("en-US", {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-      }),
-      y: row.Seattle,
-    });
+  const filteredData = monthData.filter((row) => {
+    return formatDate(row.date, "month") === targetDate;
   });
 
-  return [dataATL, dataBOS, dataPOR, dataSEA];
+  const cityProps = {
+    Atlanta: {
+      label: "Atlanta",
+      borderColor: "#34568B",
+      backgroundColor: "#34568B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Boston: {
+      label: "Boston",
+      borderColor: "#FF6F61",
+      backgroundColor: "#FF6F61",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Portland: {
+      label: "Portland",
+      borderColor: "#6B5B95",
+      backgroundColor: "#6B5B95",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Seattle: {
+      label: "Seattle",
+      borderColor: "#88B04B",
+      backgroundColor: "#88B04B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+  };
+
+  const cities = Object.keys(cityProps);
+  filteredData.forEach((row) => {
+    for (let city of cities) {
+      const formattedDate = formatDate(row.date, "day");
+      cityProps[city].data.push({
+        x: formattedDate,
+        y: row[city],
+      });
+    }
+  });
+
+  return Object.values(cityProps);
 }
 
 // Monthly filtered chart
-function monthlyFilteredChart(data, selectedMonth) {
+function monthlyFilteredChart(monthData, selectMonth) {
   // Delete disposable div
   if (document.querySelector("#disposable") !== null) {
     document.querySelector("#disposable").remove();
@@ -183,13 +197,10 @@ function monthlyFilteredChart(data, selectedMonth) {
   dispDiv.appendChild(newCanvas);
   mainDiv.appendChild(dispDiv);
 
-  // All month names
-  let months = data
+  // Month and year labels
+  const months = monthData
     .map((row) => {
-      return new Date(row.date).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-      });
+      return formatDate(row.date, "month");
     })
     .filter(uniqueValues);
 
@@ -207,7 +218,7 @@ function monthlyFilteredChart(data, selectedMonth) {
     // Add label
     option.innerHTML = month;
 
-    if (month === selectedMonth) {
+    if (month === selectMonth) {
       option.setAttribute("selected", "selected");
     }
     // Append option to list
@@ -218,14 +229,321 @@ function monthlyFilteredChart(data, selectedMonth) {
   document.querySelector("#month-group").addEventListener("change", changeMonthFilteredChart);
 
   // Set month to filter data to
-  const filteredMonth = selectedMonth === undefined ? months[0] : selectedMonth;
+  const filteredMonth = selectMonth || months[0];
 
   const monthlyChart = document.getElementById("main-chart").getContext("2d");
 
   new Chart(monthlyChart, {
     type: "line",
     data: {
-      datasets: monthData(data, filteredMonth),
+      datasets: getMonthData(monthData, filteredMonth),
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: `Daily Occupancy: ${filteredMonth}`,
+        },
+      },
+    },
+  });
+}
+
+// Historical monthly data processor
+function getHistMonthData(histData) {
+  // All month names
+  const months = histData
+    .map((row) => {
+      return formatDate(row.date, "month");
+    })
+    .filter(uniqueValues);
+
+  // Datasets for historic monthly chart
+  const cityProps = {
+    Atlanta: {
+      label: "Atlanta",
+      borderColor: "#34568B",
+      backgroundColor: "#34568B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Boston: {
+      label: "Boston",
+      borderColor: "#FF6F61",
+      backgroundColor: "#FF6F61",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Portland: {
+      label: "Portland",
+      borderColor: "#6B5B95",
+      backgroundColor: "#6B5B95",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Seattle: {
+      label: "Seattle",
+      borderColor: "#88B04B",
+      backgroundColor: "#88B04B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+  };
+
+  const cities = Object.keys(cityProps);
+
+  histData.forEach((row) => {
+    for (let city of cities) {
+      const formattedDate = formatDate(row.date, "month");
+      cityProps[city].data.push({
+        x: formattedDate,
+        y: row[city],
+      });
+    }
+  });
+
+  for (let city of cities) {
+    cityProps[city].data = cityProps[city].data.reduce((acc, cur) => {
+      if (acc[cur.x]) {
+        acc[cur.x] += cur.y;
+      } else {
+        acc[cur.x] = cur.y;
+      }
+      return acc;
+    }, {});
+  }
+
+  return Object.values(cityProps);
+}
+
+// Historical monthly chart
+function monthlyHistoricalChart(histData) {
+  // Delete disposable div
+  if (document.querySelector("#disposable") !== null) {
+    document.querySelector("#disposable").remove();
+  }
+
+  // Select main div
+  const mainDiv = document.querySelector("#main-div");
+
+  // Add dsiposable div
+  const dispDiv = document.createElement("div");
+  dispDiv.setAttribute("id", "disposable");
+
+  // Add canvas
+  const newCanvas = document.createElement("canvas");
+  newCanvas.setAttribute("id", "main-chart");
+
+  dispDiv.appendChild(newCanvas);
+  mainDiv.appendChild(dispDiv);
+
+  // All months
+  let months = histData
+    .map((row) => {
+      return new Date(row.date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+      });
+    })
+    .filter(uniqueValues);
+
+  // Historical monthly line chart
+  const histMonthlyChart = document.querySelector("#main-chart").getContext("2d");
+
+  new Chart(histMonthlyChart, {
+    type: "line",
+    data: {
+      datasets: getHistMonthData(histData),
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Historical Occupancy by Month",
+        },
+      },
+    },
+  });
+}
+
+// Monthly average data processor
+function getMonthAverageData(monthData, selectMonth) {
+  const targetDate = formatDate(selectMonth, "month");
+
+  const filteredData = monthData.filter((row) => {
+    return formatDate(row.date, "month") === targetDate;
+  });
+
+  const cityProps = {
+    Atlanta: {
+      label: "Atlanta",
+      borderColor: "#34568B",
+      backgroundColor: "#34568B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Boston: {
+      label: "Boston",
+      borderColor: "#FF6F61",
+      backgroundColor: "#FF6F61",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Portland: {
+      label: "Portland",
+      borderColor: "#6B5B95",
+      backgroundColor: "#6B5B95",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+    Seattle: {
+      label: "Seattle",
+      borderColor: "#88B04B",
+      backgroundColor: "#88B04B",
+      data: [],
+      tension: 0.1,
+      pointRadius: 0,
+      borderWidth: 2,
+    },
+  };
+
+  const cities = Object.keys(cityProps);
+  filteredData.forEach((row) => {
+    for (let city of cities) {
+      const formattedDate = formatDate(row.date, "weekday");
+      cityProps[city].data.push({
+        x: formattedDate,
+        y: row[city],
+      });
+    }
+  });
+
+  for (let city of cities) {
+    cityProps[city].data = cityProps[city].data.reduce((acc, cur) => {
+      if (acc[cur.x]) {
+        acc[cur.x] = (acc[cur.x] + cur.y) / 4;
+      } else {
+        acc[cur.x] = cur.y / 4;
+      }
+      return acc;
+    }, {});
+  }
+
+  return Object.values(cityProps);
+}
+
+// Monthly average chart
+function monthlyFilteredAverageChart(monthData, selectMonth) {
+  // Delete disposable div
+  if (document.querySelector("#disposable") !== null) {
+    document.querySelector("#disposable").remove();
+  }
+  // Select main div
+  const mainDiv = document.querySelector("#main-div");
+
+  // Add dsiposable div
+  const dispDiv = document.createElement("div");
+  dispDiv.setAttribute("id", "disposable");
+
+  // Add dropdown div
+  const dropdownDiv = document.createElement("div");
+  dropdownDiv.className = "input-group";
+  dropdownDiv.setAttribute("id", "month");
+
+  // Add label
+  const label = document.createElement("label");
+  label.className = "input-group-text";
+  label.setAttribute("for", "month-group");
+  label.innerText = "Month";
+  dropdownDiv.appendChild(label);
+
+  // Add select
+  const select = document.createElement("select");
+  select.className = "form-select";
+  select.setAttribute("id", "month-group");
+  dropdownDiv.appendChild(select);
+
+  // Add canvas
+  const newCanvas = document.createElement("canvas");
+  newCanvas.setAttribute("id", "main-chart");
+
+  // Append to main
+  dispDiv.appendChild(dropdownDiv);
+  dispDiv.appendChild(newCanvas);
+  mainDiv.appendChild(dispDiv);
+
+  // Month and year labels
+  const months = monthData
+    .map((row) => {
+      return formatDate(row.date, "month");
+    })
+    .filter(uniqueValues);
+
+  // Add months to filter
+  months.forEach((month) => {
+    // Get dropdown div
+    const monthList = document.querySelector("#month-group");
+
+    // Create option element
+    const option = document.createElement("option");
+
+    // Add value
+    option.value = month;
+
+    // Add label
+    option.innerHTML = month;
+
+    if (month === selectMonth) {
+      option.setAttribute("selected", "selected");
+    }
+    // Append option to list
+    monthList.appendChild(option);
+  });
+
+  // Add event listener
+  document.querySelector("#month-group").addEventListener("change", changeMonthFilteredAveragehart);
+
+  // Set month to filter data to
+  const filteredMonth = selectMonth || months[0];
+
+  const monthlyChart = document.getElementById("main-chart").getContext("2d");
+
+  new Chart(monthlyChart, {
+    type: "line",
+    data: {
+      datasets: getMonthAverageData(monthData, filteredMonth),
     },
     options: {
       responsive: true,
