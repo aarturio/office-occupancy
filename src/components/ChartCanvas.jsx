234743c0ff53d5a9de354981e76c4dd580ca8ddc
aarturio <106455653+aarturio@users.noreply.github.com>
2023-03-16 21:48:13 -0400

    resolve chart js issue

--- src/components/ChartCanvas.jsx
+++ src/components/ChartCanvas.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { uniqueValues, formatDate, City, drawChart } from "../helpers";
-import { Chart, Colors } from "chart.js/auto";
 import ChartData from "../data/ChartData";
 
 function ChartCanvas() {
