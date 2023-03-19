1036162e3f1195fc499401221a2758ea22b84dfe
aarturio <106455653+aarturio@users.noreply.github.com>
2023-03-18 21:19:47 -0400

    fixed issue that forced chart to re-render itself

--- src/components/ChartCanvas.jsx
+++ src/components/ChartCanvas.jsx
@@ -1,40 +1,32 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { formatDate, City } from "../helpers";
 import ChartData from "../data/ChartData";
 import { Chart } from "chart.js/auto";
 
 function ChartCanvas({ month }) {
-  const [chart, setChart] = useState(null);
   const canvasRef = useRef(null);
 
-  const targetDate = formatDate(month, "month");
-
-  const filteredData = ChartData.filter((row) => {
-    return formatDate(row.date, "month") === targetDate;
-  });
-
-  const cities = ["Atlanta", "Boston", "Portland", "Seattle"];
-  const cityProps = {};
-
-  cities.forEach((city) => {
-    cityProps[city] = new City(city);
-  });
-
-  filteredData.forEach((row) => {
-    for (let city of cities) {
-      const formattedDate = formatDate(row.date, "day");
-      cityProps[city]._data = {
-        x: formattedDate,
-        y: row[city],
-      };
-    }
-  });
-
   useEffect(() => {
-    if (chart) {
-      chart.destroy();
-    }
+    const filteredData = ChartData.filter((row) => {
+      return formatDate(row.date, "month") === formatDate(month, "month");
+    });
 
+    const cities = ["Atlanta", "Boston", "Portland", "Seattle"];
+    const cityProps = {};
+
+    cities.forEach((city) => {
+      cityProps[city] = new City(city);
+    });
+
+    filteredData.forEach((row) => {
+      for (let city of cities) {
+        const formattedDate = formatDate(row.date, "day");
+        cityProps[city]._data = {
+          x: formattedDate,
+          y: row[city],
+        };
+      }
+    });
     const ctx = canvasRef.current.getContext("2d");
 
     let newChart = null;
@@ -59,12 +51,8 @@ function ChartCanvas({ month }) {
       },
     });
 
-    setChart(newChart);
-
     return () => {
-      if (newChart) {
-        newChart.destroy();
-      }
+      newChart.destroy();
     };
   }, [month]);
 
